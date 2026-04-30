67acd431b9ca33a8b4ad0cce908ad20c7fcdaf77
Keona Truitt <156113704+truitkeo000@users.noreply.github.com>
2026-04-30 23:50:02 +0000

    Build landing page with hero section and feature cards

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Downtown Donuts' });
+});
+
+module.exports = router;
+
 /* GET home page. */
 router.get('/', function(req, res, next){
   try {
